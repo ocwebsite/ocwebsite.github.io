f03493905ac06aedde1caffd6fb26c76bb66531a
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-07 22:49:40 +0200

    omg

--- MobileBlock.js
+++ MobileBlock.js
@@ -1,12 +1,14 @@
 let popup = document.getElementById("popup")
 let topnav = document.getElementById("topnav")
 let gc = document.getElementById("grid-container")
-           // if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-          //  {
+let d = document.getElementById("device");
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+            {
                 popup.style.display="block";
                 topnav.style.display="none";
                 gc.style.display="none";
-         //   }
+            }
+            d.innerText = i.test(navigator.userAgent);
 
 const element = document.getElementById('unblock_text')
 
