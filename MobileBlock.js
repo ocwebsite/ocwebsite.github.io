0c0804f938ee4a1d25a995f34a40f6bf78361a25
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-07 21:56:36 +0200

    epic blockment

--- MobileBlock.js
+++ MobileBlock.js
@@ -1,9 +1,21 @@
+let blocked = true;
+
 let popup = document.getElementById("popup")
             let topnav = document.getElementById("topnav")
             let gc = document.getElementById("grid-container")
-            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+            if (blocked && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
             {
                 popup.style.display="block";
                 topnav.style.display="none";
                 gc.style.display="none";
             }
+
+const element = document.getElementById('unblock_text')
+
+element.addEventListener("click", () => {
+	alert('Unblocked mode activated');
+    blocked = false;
+    popup.style.display="none";
+    topnav.style.display="";
+    gc.style.display=""
+});
