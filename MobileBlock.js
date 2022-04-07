0dde9a52fa8b5b08d93a6ed97aead2ecb3dd07ab
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-07 22:21:31 +0200

    imagine

--- MobileBlock.js
+++ MobileBlock.js
@@ -1,9 +1,7 @@
-let blocked = true;
-
 let popup = document.getElementById("popup")
             let topnav = document.getElementById("topnav")
             let gc = document.getElementById("grid-container")
-            if (blocked && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
             {
                 popup.style.display="block";
                 topnav.style.display="none";
@@ -14,7 +12,6 @@ const element = document.getElementById('unblock_text')
 
 element.addEventListener("click", () => {
 	//alert('Unblocked mode activated');
-    blocked = false;
     popup.style.display="none";
     topnav.style.display="";
     gc.style.display=""
