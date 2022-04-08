ebc2d669d1ed71fc93466f211d509126df881ee9
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-08 08:19:48 +0200

    bozos

--- MobileBlock.js
+++ MobileBlock.js
@@ -2,19 +2,18 @@ let popup = document.getElementById("popup")
 let topnav = document.getElementById("topnav")
 let gc = document.getElementById("grid-container")
 let d = document.getElementById("device");
-            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+let sad_face = document.getElementById('unblock_text');
+
+            if (!navigator.userAgent.includes("Windows"))
             {
                 popup.style.display="block";
                 topnav.style.display="none";
                 gc.style.display="none";
-            }
-            d.innerText = i.test(navigator.userAgent);
+            }            
 
-const element = document.getElementById('unblock_text')
-
-element.addEventListener("click", () => {
+sad_face.addEventListener("click", () => {
 	//alert('Unblocked mode activated');
-    //popup.style.display="none";
-    //topnav.style.display="";
-    //gc.style.display=""
+    popup.style.display="none";
+    topnav.style.display="";
+    gc.style.display="";
 });
