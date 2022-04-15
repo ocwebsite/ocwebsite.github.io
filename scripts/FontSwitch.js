aa57b6479e842db44b85c59410e0c793ca4aea53
Vitsowdy <56477530+Vitsowdy@users.noreply.github.com>
2022-04-15 22:41:58 +0300

    Font changer

---
+++ scripts/FontSwitch.js
@@ -0,0 +1,9 @@
+let body = document.getElementById("body")
+let checkbox = document.getElementById("fontchange")
+
+checkbox.addEventListener("click", () =>{
+    if (checkbox.checked)
+        body.style.fontFamily = "Times New Roman";
+    else
+        body.style.fontFamily = "'Roboto', sans-serif";
+});
