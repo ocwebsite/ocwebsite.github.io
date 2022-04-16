f4a90eff7f0be91ae8abdaf5336f690ed123c088
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-16 22:49:40 +0200

    test

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -8,9 +8,14 @@ function readFile(pathOfFileToReadFrom)
         return returnValue;
     }
 
+    var text2 = readFile("./misc/changelog.txt");
+
+function getLog(){
 var text = readFile("./misc/changelog.txt");
 
 var des = document.getElementById("descr");
 
-alert(text)
 des.setAttribute("content", text)
+}
+alert(text2)
+
