5907cfc5cdc16c78ed7a815b8f0282e0687bb679
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-16 23:03:51 +0200

    omh

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -8,14 +8,13 @@ function readFile(pathOfFileToReadFrom)
         return returnValue;
     }
 
-    var text2 = readFile("./misc/changelog.txt");
 
-function getLog(){
 var text = readFile("./misc/changelog.txt");
 
 var des = document.getElementById("descr");
+var head = document.getElementById("head");
 
-des.setAttribute("content", text)
-}
-alert(text2)
+head.innerHTML = '<script src="scripts/changelog.js"></script> <title>Onix Client</title> <link rel="stylesheet" href="styles/style_main.css"> <link rel="stylesheet" href="styles/dropdown.css"><meta content="Onix Client Changelog" property="og:title"><meta content="Onix Client" property="og:site_name"><meta id="descr" content="'+text+'" property="og:description"><meta content="https://ocwebsite.github.io/changelog" property="og:url"><meta content="https://ocwebsite.github.io/assets/banner.jpg" property="og:image"><meta content="summary_large_image" name="twitter:card"><meta content="#008FC1" data-react-helmet="true" name="theme-color">'
+
+alert(text)
 
