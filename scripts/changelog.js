db1f3c020054127beae1b921468915941e695745
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-16 22:18:55 +0200

    yes

---
+++ scripts/changelog.js
@@ -0,0 +1,18 @@
+function FileHelper()
+{
+    FileHelper.readStringFromFileAtPath = function(pathOfFileToReadFrom)
+    {
+        var request = new XMLHttpRequest();
+        request.open("GET", pathOfFileToReadFrom, false);
+        request.send(null);
+        var returnValue = request.responseText;
+
+        return returnValue;
+    }
+}
+
+var text = FileHelper.readStringFromFileAtPath("./misc/changelog.txt");
+
+var des = document.getElementById("descr");
+
+des.textContent = text;
