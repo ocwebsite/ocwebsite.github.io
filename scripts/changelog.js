e0ee881621baf811e6da43888e5dbafe605822b9
EpiclyRaspberry <84133368+EpiclyRaspberry@users.noreply.github.com>
2022-04-17 05:21:42 +0800

    bloce width

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -15,5 +15,9 @@ var des = document.getElementById("descr");
 var head = document.getElementById("header");
 document.innerHTML.head.innerHTML = '<title>Onix Client</title> <link rel="stylesheet" href="styles/style_main.css"> <link rel="stylesheet" href="styles/dropdown.css"><meta content="Onix Client Changelog" property="og:title"><meta content="Onix Client" property="og:site_name"><meta id="descr" content="'+text+'" property="og:description"><meta content="https://ocwebsite.github.io/changelog" property="og:url"><meta content="https://ocwebsite.github.io/assets/banner.jpg" property="og:image"><meta content="summary_large_image" name="twitter:card"><meta content="#008FC1" data-react-helmet="true" name="theme-color">'
 
+var embed = document.getElementById("changelog-embed");
+
+embed.width = window.innerWidth;
+
 alert(text)
 
