0433eaeffbd472bfd0bd42801955aab58e13b09d
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-17 08:44:14 +0200

    FIX THE CHANGELOG LOADING!!!!!!!!!!

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -9,16 +9,20 @@ function readFile(pathOfFileToReadFrom)
     }
 
 
-var text = readFile("./misc/changelog.txt");
+var text = "ok"//readFile("./misc/changelog.txt");
 
-var des = document.getElementById("descr");
 var head = document.getElementById("header");
+var changelogtext = document.getElementsByClassName("changelog");
 
 document.head.innerHTML = '<title>Onix Client</title> <link rel="stylesheet" href="styles/style_main.css"> <link rel="stylesheet" href="styles/dropdown.css"><meta content="Onix Client Changelog" property="og:title"><meta content="Onix Client" property="og:site_name"><meta id="descr" content="'+text+'" property="og:description"><meta content="https://ocwebsite.github.io/changelog" property="og:url"><meta content="https://ocwebsite.github.io/assets/banner.jpg" property="og:image"><meta content="summary_large_image" name="twitter:card"><meta content="#008FC1" data-react-helmet="true" name="theme-color">'
 
-var embed = document.getElementById("changelog-embed");
+if (changelogtext == null){
+    alert("FFFFFFFFFFFFFFF")
+}
 
-embed.width = window.innerWidth;
+window.onload = function(){
+changelogtext[0].textContent = "yes?";
+}
 
 alert(text)
 
