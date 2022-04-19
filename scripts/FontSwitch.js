41a40393da6a25f0d655305806f68d9d0392cb45
Vitsowdy <56477530+Vitsowdy@users.noreply.github.com>
2022-04-19 22:09:58 +0300

    Removed fontchanger

--- scripts/FontSwitch.js
+++
@@ -1,9 +0,0 @@
-let body = document.getElementById("body")
-let checkbox = document.getElementById("fontchange")
-
-checkbox.addEventListener("click", () =>{
-    if (checkbox.checked)
-        body.style.fontFamily = "Times New Roman";
-    else
-        body.style.fontFamily = "'Roboto', sans-serif"; //more fonts when?
-});
