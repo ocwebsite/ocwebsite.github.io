f861538b84da6cdd67c0fcc880835474b6f261c2
Vitsowdy <56477530+Vitsowdy@users.noreply.github.com>
2022-04-20 21:09:14 +0300

    Changed import of stylesheet in pages

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -22,7 +22,7 @@ for (let i=0;i<text3.length;i++){
     text = text + text3[i] + "\n";
 }
 
-document.head.innerHTML = '<title>Onix Client</title> <link rel="stylesheet" href="styles/style_main.css"> <link rel="stylesheet" href="styles/dropdown.css"><meta content="Onix Client Changelog" property="og:title"><meta content="Onix Client" property="og:site_name"><meta id="descr" content="'+text+'" property="og:description"><meta content="https://ocwebsite.github.io/changelog" property="og:url"><meta content="https://ocwebsite.github.io/assets/banner.jpg" property="og:image"><meta content="summary_large_image" name="twitter:card"><meta content="#008FC1" data-react-helmet="true" name="theme-color">'
+document.head.innerHTML = '<title>Onix Client</title> <link rel="stylesheet" href="styles/main.min.css"> <link rel="stylesheet" href="styles/dropdown.css"><meta content="Onix Client Changelog" property="og:title"><meta content="Onix Client" property="og:site_name"><meta id="descr" content="'+text+'" property="og:description"><meta content="https://ocwebsite.github.io/changelog" property="og:url"><meta content="https://ocwebsite.github.io/assets/banner.jpg" property="og:image"><meta content="summary_large_image" name="twitter:card"><meta content="#008FC1" data-react-helmet="true" name="theme-color">'
 
 if (changelogtext == null){
     alert("FFFFFFFFFFFFFFF")
