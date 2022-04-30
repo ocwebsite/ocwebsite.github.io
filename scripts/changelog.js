2a62453356907fc0a2ea6d2d81e2aa9ad52f1ecd
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-30 11:25:23 +0200

    i hate

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -21,6 +21,11 @@ text3 = text2.split("[n]")
 for (let i=0;i<text3.length;i++){
     text = text + text3[i] + "\n";
 }
+
 window.onload = function(){
+    changelogtext[0].innerHTML = text
+}
+
+head.onload = function(){
     head.setAttribute("content", text)
 }
