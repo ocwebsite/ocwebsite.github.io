3e25057d5f0c43f27a4656e1d6081e0fad0eac5f
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-30 11:01:53 +0200

    cat on my keyboard

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -11,7 +11,7 @@ function readFile(pathOfFileToReadFrom)
 
 var text2 = readFile("./misc/changelog.txt");
 
-var head = document.getElementById("header");
+var head = document.getElementById("descr");
 var changelogtext = document.getElementsByClassName("changelog");
 
 var text = "";
@@ -21,16 +21,6 @@ text3 = text2.split("[n]")
 for (let i=0;i<text3.length;i++){
     text = text + text3[i] + "\n";
 }
-
-document.head.innerHTML = '<title>Onix Client</title> <link rel="stylesheet" href="styles/main.min.css"> <link rel="stylesheet" href="styles/dropdown.css"><meta content="Onix Client Changelog" property="og:title"><meta content="Onix Client" property="og:site_name"><meta id="descr" content="'+text+'" property="og:description"><meta content="https://ocwebsite.github.io/changelog" property="og:url"><meta content="https://ocwebsite.github.io/assets/banner.jpg" property="og:image"><meta content="summary_large_image" name="twitter:card"><meta content="#008FC1" data-react-helmet="true" name="theme-color">'
-
-if (changelogtext == null){
-    alert("FFFFFFFFFFFFFFF")
-}
-
 window.onload = function(){
-changelogtext[0].textContent = text;
+    head.setAttribute("content", text)
 }
-
-alert(text)
-
