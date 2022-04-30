7a9a1a3b01eb830e4a55e3ac88b7e97825ad605c
Hmmm <100090034+h-m-m-m@users.noreply.github.com>
2022-04-30 11:30:58 +0200

    cum

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -11,7 +11,7 @@ function readFile(pathOfFileToReadFrom)
 
 var text2 = readFile("./misc/changelog.txt");
 
-var head = document.getElementById("descr");
+var _head = document.getElementById("descr");
 var changelogtext = document.getElementsByClassName("changelog");
 
 var text = "";
@@ -26,6 +26,6 @@ window.onload = function(){
     changelogtext[0].innerHTML = text
 }
 
-head.onload = function(){
-    head.setAttribute("content", text)
+_head.onload = function(){
+    _head.setAttribute("content", text)
 }
